fb55d346ac40a4205574c227ae0f0313b44eda4e
Alexander Volotchin <alexrazek@yandex.ru>
2023-06-14 00:11:59 +0300

    sprint_13 test_8

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ const app = express();
 //   res.send('hellos gigi');
 // });
 
-const routerUser = require('./routes/users');
-const routerCard = require('./routes/cards');
-
-const { PORT = 3000 } = process.env;
-
-// подключаемся к серверу mongo
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
 app.use((req, res, next) => {
   req.user = {
     _id: '64863f8b1e71d6c164ddbd1a', // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -31,7 +17,28 @@ app.use((req, res, next) => {
   next();
 });
 
+const routerUser = require('./routes/users');
+const routerCard = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+// app.use((req, res, next) => {
+//   req.user = {
+//   _id: '64863f8b1e71d6c164ddbd1a', // вставьте сюда _id созданного в предыдущем пункте пользовате
+//   };
+//   next();
+// });
+
 app.use('/users', routerUser);
 app.use('/cards', routerCard);
 
